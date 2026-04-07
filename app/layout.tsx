cbc400963f2b8c6bc50289952a6a779260abe4f8
Hoang Nghi <hoangnghinguyen17@gmail.com>
2026-04-07 18:49:04 +0700

    change global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="vi"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="bg-white min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
