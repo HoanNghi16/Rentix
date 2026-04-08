aefd502f2d58eb6843ba117331bd907be5ca076c
Hoang Nghi <hoangnghinguyen17@gmail.com>
2026-04-08 20:50:34 +0700

    merge pink (02)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./components/login/loginform";
 export default function Login() {
   return (
     <div className="bg-gradient-to-br from-blue-50 to-blue-200 text-black p-10 w-full h-screen py-30">
-      <LoginForm />
+      <LoginForm />::
     </div>
   );
 }
